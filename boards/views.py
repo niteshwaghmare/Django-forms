f010162cbc4d889054051cdb94555623f5f77fcd
Nitesh Waghmare <niteshw2@gmail.com>
2020-08-25 18:45:06 +0530

    More improvement

--- boards/views.py
+++ boards/views.py
@@ -1,37 +1,171 @@
-from .models import Board
-from django.http import HttpResponse
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
-from django.contrib.auth.models import User 
-from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.db.models import Count
+from django.urls import reverse_lazy
+from .models import Board, Post, Topic
+from .forms import NewTopicForm, PostForm
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import CreateView, UpdateView, ListView
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards': boards})
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
 
 
-def board_topics(request, pk):
-    board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board': board})
+# def board_topics(request, pk):
+#     board = get_object_or_404(Board, pk=pk)
+#     queryset = board.topics.order_by(
+#         '-last_updated').annotate(replies=Count('posts') - 1)
+#     page = request.GET.get('page')
+
+#     paginator = Paginator(queryset, 10)
+
+#     try:
+#         topics = paginator.page(page)
+#     except PageNotAnInteger:
+#         # fallback to the first page
+#         topics = paginator.page(1)
+#     except EmptyPage:
+#         # probably the user tried to add a page number
+#         # in the url, so we fallback to the last page
+#         topics = paginator.page(paginator.num_pages)
+
+#     return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'topics.html'
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        kwargs['board'] = self.board
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+        queryset = self.board.topics.order_by(
+            '-last_updated').annotate(replies=Count('posts') - 1)
+        return queryset
+
+
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
-            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    return render(request, 'topic_posts.html', {'topic': topic})
+
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
+    return render(request, 'topic_posts.html', {'topic': topic})
+
+
+'''
+**** Funcation based views ****
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
+
+
+re_path(r'^new_post/$', views.new_post, name='new_post'),
+
+'''
+
+'''
+
+======== Class based view ===========
+
+class NewPostView(View):
+    def render(self, request):
+        return render(request, 'new_post.html', {'form': self.form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+        return render(request, 'new_post.html', {'form': form})
+
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'new_post.html', {'form': form})
+
+re_path(r'^new_post/$', views.NewPostView.as_view(), name='new_post'),
+
+'''
+
+# Generic Class-Based View
+
+
+class NewPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('post_list')
+    template_name = 'new_post.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
